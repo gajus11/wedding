87c875fde1d7aafd9818c20d5c843b221b82fe86
lgajownik <gajus11@gmail.com>
2016-07-21 10:58:52 +0200

    Change admin view for Gift

--- gifts/admin.py
+++ gifts/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Gift
 
+class GiftAdmin(admin.ModelAdmin):
+    list_display = ['name' ,'image', 'link',
+                    'is_reserved', 'reserver']
+
 # Register your models here.
-admin.site.register(Gift)
+admin.site.register(Gift, GiftAdmin)
